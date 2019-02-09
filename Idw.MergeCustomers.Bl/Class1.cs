59dc45c455300726ac838135dfbbdd79bfe96afc
Xavier Acosta <xavieracosta@ingenieros.com>
2019-02-08 19:45:50 -0500

    Plantilla inicial de la solucion

---
+++ Idw.MergeCustomers.Bl/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idw.MergeCustomers.Bl
+{
+    public class Class1
+    {
+    }
+}
---
+++ Idw.MergeCustomers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Idw.MergeCustomers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Idw.MergeCustomers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c68a242-7316-4b29-b718-692995e30b83")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
